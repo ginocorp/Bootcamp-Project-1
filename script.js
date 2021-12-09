0f3d12073a073510a09cad0fc87c07099578c226
Naza Okafor <91860335+nazaokafor@users.noreply.github.com>
2021-12-09 01:27:43 -0500

    added functionality to html buttons

--- script.js
+++ script.js
@@ -1,4 +1,19 @@
-var newMusic = document.querySelector("#new-music");
-clickButton.addEventListener("click", function() {
+var getNewMusicButtonEl = $('#get-new-music-btn');
+var dateRangeButtonEl = $('#date-range-btn');
+var musicGenreButtonEl = $('#music-genre-btn');
 
+var containerEl = $('#music-containter');
+
+getNewMusicButtonEl.on('click', function() {
+    console.log('I was clicked to generate new music...');
+    alert('Type on the search bar to look up the latest hits on music charts!');
+});
+
+dateRangeButtonEl.on('click', function() {
+    console.log('I was clicked to generate a range of dates...');
+    
+});
+
+musicGenreButtonEl.on('click', function() {
+    console.log('I was clicked to generate music genres...');
 });
